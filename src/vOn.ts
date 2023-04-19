aac0bb8dc1d8ecd09137ea3c51d13fde21a1a575
zhaogongchengsi <zzh1586169624@163.com>
2023-04-19 17:59:11 +0800

    todo: 监听常用快捷键

--- src/vOn.ts
+++ src/vOn.ts
@@ -1,17 +1,52 @@
 import { type Oel, querySelector } from "./utils";
 
-export function on(oel: Oel, e: string, handler: EventListener, optios?: EventListenerOptions) {
+const systemModifiers = ["ctrl", "shift", "alt", "meta"];
+
+const _on = (
+  oel: Oel,
+  e: string,
+  handler: EventListener,
+  optios?: EventListenerOptions,
+) => {
   const el = querySelector(oel);
   if (!el) {
     return;
   }
   el.addEventListener(e, handler, optios);
-}
+  return () => {
+    el.removeEventListener(e, handler, optios);
+  };
+};
 
-export function off(oel: Oel, e: string, handler: EventListener, optios?: EventListenerOptions) {
+const _off = (
+  oel: Oel,
+  e: string,
+  handler: EventListener,
+  optios?: EventListenerOptions,
+) => {
   const el = querySelector(oel);
   if (!el) {
     return;
   }
   el.removeEventListener(e, handler, optios);
+};
+
+export function on(
+  oel: Oel,
+  e: string,
+  handler: EventListener,
+  optios?: EventListenerOptions,
+) {
+  return _on(oel, e, handler, optios);
+}
+
+// todo: 监听快捷键
+
+export function off(
+  oel: Oel,
+  e: string,
+  handler: EventListener,
+  optios?: EventListenerOptions,
+) {
+  _off(oel, e, handler, optios);
 }
